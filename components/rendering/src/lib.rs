1b332d07c3ad6acff244b5b581473662c39ff7a1
Micah Cantor <micahcantor01@gmail.com>
2021-07-21 23:41:39 -0400

    render katex if option is turned on

--- components/rendering/src/lib.rs
+++ components/rendering/src/lib.rs
@@ -2,6 +2,7 @@ mod codeblock;
 mod context;
 mod markdown;
 mod shortcode;
+mod extract_katex;
 mod table_of_contents;
 
 use errors::Result;
@@ -9,12 +10,19 @@ use errors::Result;
 pub use context::RenderContext;
 use markdown::markdown_to_html;
 pub use shortcode::render_shortcodes;
+pub use extract_katex::render_katex;
 pub use table_of_contents::Heading;
 
 pub fn render_content(content: &str, context: &RenderContext) -> Result<markdown::Rendered> {
     // Don't do shortcodes if there is nothing like a shortcode in the content
     if content.contains("{{") || content.contains("{%") {
         let rendered = render_shortcodes(content, context)?;
+        let html = markdown_to_html(&rendered, context)?;
+        return Ok(html);
+    }
+
+    if context.config.markdown.katex {
+        let rendered = render_katex(content);
         let html = markdown_to_html(&rendered, context)?;
         return Ok(html);
     }
